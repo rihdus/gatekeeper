a17c52af8a1d66b867ba0fe40feb54e02268eebd
Rihdus <rihdus.io@gmail.com>
2019-04-20 16:35:55 +0800

    feat: First commit. Login, registation, commit setup
    New public facing routes for login and registration apis.
    Implements a basic authentication system using in memory store.

---
+++ README.md
@@ -0,0 +1,10 @@
+Gatekeeper
+==========
+
+Authentication and authorisation service.
+
+## Registration
+Create an account for the user in the system.
+
+## Login
+Identify user via username and password and create a session for the user.
---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const usersRoute = require('../api').users;
+
+app.use(express.json());
+//use users route for api/users
+app.use("/api/users", usersRoute);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
